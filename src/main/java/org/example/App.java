3f054ed847123de3f1dc46f68fc1b79d6701df5a
Alex Barin <A1exkillerbunny@gmail.com>
2024-12-12 21:57:13 +0300

    final yess

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,7 +4,7 @@ public class App {
     public int add(int a, int b) {
         return a + b;
     }
-//final? no
+//final? yeeeees
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
