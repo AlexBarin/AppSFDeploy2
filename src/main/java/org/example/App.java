f4bcff0311118efbec8babd82fbadd1845f2d113
Alex Barin <A1exkillerbunny@gmail.com>
2024-12-12 21:07:19 +0300

    iii

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,14 +4,7 @@ public class App {
     public int add(int a, int b) {
         return a + b;
     }
-//coment test
-//second test
-//next try
-//hope no
-//last zap
-//yaaaaaaaaaaaaaaaaaaaaaaaaa ijooooo
-
-
+//ddd
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
